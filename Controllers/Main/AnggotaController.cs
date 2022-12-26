4f8ce34f06cb3c9be1852871c934b7ccedff0a74
itdinaslh <itdlh@outlook.com>
2022-12-26 21:43:36 +0700

    Update Banyak

---
+++ Controllers/Main/AnggotaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Koperasi.Controllers;
+
+
+public class AnggotaController : Controller
+{
+    [HttpGet("/anggota/list")]
+	[Authorize(Roles = "SysAdmin, KoperasiAdmin")]
+	public IActionResult List()
+    {
+        return View("~/Views/Main/Anggota/List.cshtml");
+    }
+}
---
+++ wwwroot/pages/main/anggota/list.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    loadTable();
+});
+
+function loadContent() {
+    loadTable();
+}
+
+function loadTable() {
+    $('#tblData').DataTable({
+        language: {
+            paginate: {
+                next: '<i class="fa fa-angle-double-right" aria-hidden="true"></i>',
+                previous: '<i class="fa fa-angle-double-left" aria-hidden="true"></i>'
+            }
+        }
+    });
+}
